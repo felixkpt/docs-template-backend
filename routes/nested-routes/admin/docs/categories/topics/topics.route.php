1fc62f0e4df680201de02496588323b65a649a0d
felixkpt <felixkpt@gmail.com>
2023-09-15 09:06:48 +0300

    saving doc changes

--- routes/nested-routes/admin/docs/categories/topics/topics.route.php
+++ routes/nested-routes/admin/docs/categories/topics/topics.route.php
@@ -4,8 +4,8 @@ use App\Http\Controllers\Admin\DocumentationPages\Categories\Topics\TopicsContro
 use Illuminate\Support\Facades\Route;
 
 $controller = TopicsController::class;
-Route::get('/', [$controller, 'index'])->name('Topics List')->hidden();
+Route::get('/', [$controller, 'index'])->name('Topics List')->everyone()->hidden();
 Route::get('/create', [$controller, 'create'])->name('Create topic')->hidden();
 Route::post('/', [$controller, 'store'])->name('Store topic');
 Route::put('/{id}', [$controller, 'update'])->name('Update topic');
-Route::get('/detail/{id}', [$controller, 'show'])->name('Show topic');
+Route::get('/detail/{id}', [$controller, 'show'])->name('Show topic')->everyone();
